af3b1936c3e51b4554477fa1bbeea21c5563157d
RTalate <rayan.talate@ensae.fr>
2023-12-21 14:23:46 +0100

    Update Colorization.py
    80 Epochs; suppression de fonctions inutiles ; ajout d'un dropout.

--- Colorization.py
+++ Colorization.py
@@ -17,13 +17,13 @@ import os
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-if torch.cuda.is_available():
-  print(torch.cuda.current_device())
-  print(torch.cuda.device(0))
-  print(torch.cuda.device_count())
-  print(torch.cuda.get_device_name(0))
-else:
-  print("No NVIDIA driver found. Using CPU")
+# if torch.cuda.is_available():
+#   print(torch.cuda.current_device())
+#   print(torch.cuda.device(0))
+#   print(torch.cuda.device_count())
+#   print(torch.cuda.get_device_name(0))
+# else:
+#   print("No NVIDIA driver found. Using CPU")
 
 #%% Load the CIFAR-10 dataset
 img_path=r"C:\Users\rayan\Documents\GitHub\Projet infra\images folder\color"
@@ -57,16 +57,19 @@ if __name__=="__main__":
     class ColorizationNet(nn.Module):
         def __init__(self):
             super(ColorizationNet, self).__init__()
-            self.conv1 = nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=4, dilation=2)
-            self.conv2 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=4, dilation=2)
-            self.conv3 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=4, dilation=2)
-            self.conv4 = nn.Conv2d(128, 3, kernel_size=5, stride=1, padding=4, dilation=2)
+            self.conv1 = nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=4, dilation=2) #grayscale to 64 feature maps
+            self.conv2 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=4, dilation=2) #64 to 64
+            self.conv3 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=4, dilation=2)# increase of feature maps to capture more complex patterns
+            self.conv4 = nn.Conv2d(128, 3, kernel_size=5, stride=1, padding=4, dilation=2) # 3 channels: RGB
+            self.dropout = nn.Dropout(0.1)
     
         def forward(self, x):
+            x = self.dropout(x)
             x = nn.functional.relu(self.conv1(x))
             x = nn.functional.relu(self.conv2(x))
             x = nn.functional.relu(self.conv3(x))
             x = torch.sigmoid(self.conv4(x))
+            
             return x
     
     model = ColorizationNet().to(device)
@@ -80,7 +83,7 @@ if __name__=="__main__":
         return img.mean(dim=1, keepdim=True)
     
     # Training loop
-    EPOCHS = 30
+    EPOCHS = 80
     for epoch in range(EPOCHS):
         for i, (images, _) in enumerate(train_loader):
             grayscale_images = rgb_to_gray(images).to(device)
@@ -101,8 +104,6 @@ if __name__=="__main__":
     
     print("Finished Training")
 #%%
-    
-
     def imshow(img):
         # Convert from Tensor image and display
         img = img / 2 + 0.5  # Unnormalize
@@ -112,174 +113,10 @@ if __name__=="__main__":
         else:
             plt.imshow(np.transpose(npimg, (1, 2, 0)))
     
-    def visualize_all_three(original_images, grayscale_images, colorized_images, n=5):
-        """
-        Display grayscale, colorized, and original images side by side.
-        n: number of images to display from the batch
-        """
-        fig = plt.figure(figsize=(3*n, 4))
-        for i in range(n):
-            # Display original image
-            ax = plt.subplot(1, 3*n, 3*i + 1)
-            imshow(original_images[i])
-            ax.set_title("Original")
-            ax.axis("off")
-    
-            # Display original grayscale image
-            ax = plt.subplot(1, 3*n, 3*i + 2)
-            imshow(grayscale_images[i])
-            ax.set_title("Grayscale")
-            ax.axis("off")
-    
-            # Display colorized image
-            ax = plt.subplot(1, 3*n, 3*i + 3)
-            imshow(colorized_images[i])
-            ax.set_title("Colorized")
-            ax.axis("off")
-    
-        plt.tight_layout()
-        plt.show()
-    
-    
-    def torch_rgb_to_hsv(rgb):
-        """
-        Convert an RGB image tensor to HSV.
-    
-        Parameters:
-        - rgb: tensor of shape (batch_size, 3, height, width) in RGB format in the range [0, 1].
-    
-        Returns:
-        - hsv: tensor of same shape in HSV format in the range [0, 1].
-        """
-        r, g, b = rgb[:, 0, :, :], rgb[:, 1, :, :], rgb[:, 2, :, :]
-        max_val, _ = torch.max(rgb, dim=1)
-        min_val, _ = torch.min(rgb, dim=1)
-        diff = max_val - min_val
-    
-        # Compute H
-        h = torch.zeros_like(r)
-        mask = (max_val == r) & (g >= b)
-        h[mask] = (g[mask] - b[mask]) / diff[mask]
-        mask = (max_val == r) & (g < b)
-        h[mask] = (g[mask] - b[mask]) / diff[mask] + 6.0
-        mask = max_val == g
-        h[mask] = (b[mask] - r[mask]) / diff[mask] + 2.0
-        mask = max_val == b
-        h[mask] = (r[mask] - g[mask]) / diff[mask] + 4.0
-        h = h / 6.0
-        h[diff == 0.0] = 0.0
-    
-        # Compute S
-        s = torch.zeros_like(r)
-        s[diff != 0.0] = diff[diff != 0.0] / max_val[diff != 0.0]
-    
-        # V is just max_val
-        v = max_val
-    
-        return torch.stack([h, s, v], dim=1)
-    
-    
-    def torch_hsv_to_rgb(hsv):
-        """
-        Convert an HSV image tensor to RGB.
-    
-        Parameters:
-        - hsv: tensor of shape (batch_size, 3, height, width) in HSV format in the range [0, 1].
-    
-        Returns:
-        - rgb: tensor of same shape in RGB format in the range [0, 1].
-        """
-        h, s, v = hsv[:, 0, :, :], hsv[:, 1, :, :], hsv[:, 2, :, :]
-        i = (h * 6.0).floor()
-        f = h * 6.0 - i
-        p = v * (1.0 - s)
-        q = v * (1.0 - s * f)
-        t = v * (1.0 - s * (1.0 - f))
-    
-        i_mod = i % 6
-        r = torch.zeros_like(h)
-        g = torch.zeros_like(h)
-        b = torch.zeros_like(h)
-    
-        r[i_mod == 0.0] = v[i_mod == 0.0]
-        g[i_mod == 0.0] = t[i_mod == 0.0]
-        b[i_mod == 0.0] = p[i_mod == 0.0]
-    
-        r[i_mod == 1.0] = q[i_mod == 1.0]
-        g[i_mod == 1.0] = v[i_mod == 1.0]
-        b[i_mod == 1.0] = p[i_mod == 1.0]
-    
-        r[i_mod == 2.0] = p[i_mod == 2.0]
-        g[i_mod == 2.0] = v[i_mod == 2.0]
-        b[i_mod == 2.0] = t[i_mod == 2.0]
-    
-        r[i_mod == 3.0] = p[i_mod == 3.0]
-        g[i_mod == 3.0] = q[i_mod == 3.0]
-        b[i_mod == 3.0] = v[i_mod == 3.0]
-    
-        r[i_mod == 4.0] = t[i_mod == 4.0]
-        g[i_mod == 4.0] = p[i_mod == 4.0]
-        b[i_mod == 4.0] = v[i_mod == 4.0]
-    
-        r[i_mod == 5.0] = v[i_mod == 5.0]
-        g[i_mod == 5.0] = p[i_mod == 5.0]
-        b[i_mod == 5.0] = q[i_mod == 5.0]
-    
-        return torch.stack([r, g, b], dim=1)
-    
-    def exaggerate_colors(images, saturation_factor=1.5, value_factor=1.2):
-        """
-        Exaggerate the colors of RGB images.
-    
-        Parameters:
-        - images: tensor of shape (batch_size, 3, height, width) in RGB format.
-        - saturation_factor: factor by which to increase the saturation. Default is 1.5.
-        - value_factor: factor by which to increase the value/brightness. Default is 1.2.
-    
-        Returns:
-        - color_exaggerated_images: tensor of same shape as input, with exaggerated colors.
-        """
-        # Convert images to the range [0, 1]
-        images = (images + 1) / 2.0
-    
-        # Convert RGB images to HSV
-        images_hsv = torch_rgb_to_hsv(images)
-    
-        # Increase the saturation and value components
-        images_hsv[:, 1, :, :] = torch.clamp(images_hsv[:, 1, :, :] * saturation_factor, 0, 1)
-        images_hsv[:, 2, :, :] = torch.clamp(images_hsv[:, 2, :, :] * value_factor, 0, 1)
-    
-        # Convert the modified HSV images back to RGB
-        color_exaggerated_images = torch_hsv_to_rgb(images_hsv)
-    
-        # Convert images back to the range [-1, 1]
-        color_exaggerated_images = color_exaggerated_images * 2.0 - 1.0
-    
-        return color_exaggerated_images
-    
-    with torch.no_grad():
-        for i, (images, _) in enumerate(test_loader):
-            grayscale_images = rgb_to_gray(images).to(device)
-            colorized_images = model(grayscale_images)
-    
-            # Convert the tensors back to CPU for visualization
-            grayscale_images_cpu = grayscale_images.cpu().squeeze(1)  # remove the color channel
-            colorized_images_cpu = colorized_images.cpu()
-            original_images_cpu = images.cpu()
-    
-            #colorized_images_cpu=scale_predicted_colors(colorized_images_cpu)
-            colorized_images_cpu=exaggerate_colors(colorized_images_cpu)
-    
-            # Visualize the grayscale, colorized, and original images
-            # visualize_all_three(original_images_cpu, grayscale_images_cpu, colorized_images_cpu)
-    
-            if i == 10:  # only do this for up to certain batch for demonstration purposes
-                break
-    
     
 #%%
     # Open the image. (Keep your image in the current directory. In my case, the image was horse.jpg)
-    img = Image.open(r"C:\Users\rayan\Documents\GitHub\Projet infra\images folder\montagne-ete-scaled.jpg")
+    img = Image.open(r"C:\Users\rayan\Documents\GitHub\Projet infra\images folder\screen.jpg")
     
     # Convert the image to grayscale
     gray_img = img.convert("L")
@@ -287,8 +124,6 @@ if __name__=="__main__":
     # Define the transformations
     transform = transforms.Compose([
         transforms.ToTensor(),
-        # If you need to normalize, uncomment the following line
-        # transforms.Normalize(mean=[0.5], std=[0.5])  # Assuming you want to normalize to [-1, 1] range
     ])
     
     # Apply the transformations
